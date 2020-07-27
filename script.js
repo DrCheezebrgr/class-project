c7eaf6ca1c1e78bcf37d91869867c400e30c03cb
Craig Cox <feheart87@gmail.com>
2020-07-27 19:37:33 -0400

    confirmation for email

--- script.js
+++ script.js
@@ -28,3 +28,51 @@ function showSlides(n) {
   dots[slideIndex-1].className += " active";
   captionText.innerHTML = dots[slideIndex-1].alt;
 }
+
+const form  = document.getElementsByTagName('form')[0];
+
+const email = document.getElementById('mail');
+const emailError = document.querySelector('#mail + span.error');
+
+email.addEventListener('input', function (event) {
+  // Each time the user types something, we check if the
+  // form fields are valid.
+
+  if (email.validity.valid) {
+    // In case there is an error message visible, if the field
+    // is valid, we remove the error message.
+    // Reset the content
+    emailError.innerHTML = '';
+    // Reset the visual
+    emailError.className = 'error';
+  } else {
+    // If error persists
+    showError();
+  }
+});
+
+form.addEventListener('submit', function (event) {
+  // if the email valid
+
+  if(!email.validity.valid) {
+    // If not
+    showError();
+    event.preventDefault();
+  }
+});
+
+function showError() {
+  if(email.validity.valueMissing) {
+    // If field empty display this
+    emailError.textContent = 'You need to enter an e-mail address.';
+  } else if(email.validity.typeMismatch) {
+    // If no emial display this
+    emailError.textContent = 'Entered value needs to be an e-mail address.';
+  } else if(email.validity.tooShort) {
+    // If too short display this
+    emailError.textContent = `Email should be at least ${ email.minLength } characters; you entered ${ email.value.length }.`;
+  }
+
+ 
+  emailError.className = 'error active';
+}
